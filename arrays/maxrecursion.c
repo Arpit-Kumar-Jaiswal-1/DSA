01bee133d879a7404a08fe5a93d18a3dec4fb009
Arpit-Kumar-Jaiswal-1 <arpit1.mitmpl2024@learner.manipal.edu>
2025-08-23 03:06:05 +0530

    Add files via upload

---
+++ arrays/maxrecursion.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+int Max(int arr[] ,int n,int max){
+   
+    if(n==0){
+        return max;
+    }
+    if(arr[n-1]>max){
+        max=arr[n-1];
+    }
+   
+  return Max(arr,  n-1, max);
+
+}
+/*Non-Accumulator 
+int findMax(int arr[], int n) {
+    if (n == 1) return arr[0];          
+    int maxRest = findMax(arr+1, n-1);  
+    return (arr[0] > maxRest) ? arr[0] : maxRest;
+}
+
+*/
+int main(){
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int *arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int maxi = -100000; // Minimum value for a signed int
+
+    int max = Max(arr, n,maxi);
+    printf("Maximum element is: %d\n", max);
+
+    free(arr);
+
+
+
+
+}
